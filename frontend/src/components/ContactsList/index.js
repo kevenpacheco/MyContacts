bad9b298d59db0ea973dd4851db88c41883e9e62
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-03-05 19:17:58 -0300

    create the contact list

--- frontend/src/components/ContactsList/index.js
+++ frontend/src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Card, Container, Header, ListContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export function ContactsList() {
   return (
@@ -7,6 +13,35 @@ export function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Keven Pacheco</strong>
+              <small>instagram</small>
+            </div>
+            <span>kevenpacheco2000@gmail.com</span>
+            <span>(31) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }

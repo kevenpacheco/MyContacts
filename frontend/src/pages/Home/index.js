382d6cbeb10fc90eda53998029959dcb6a3be332
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-03-06 17:42:17 -0300

    create the Loader component

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -5,12 +5,12 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import { Modal } from '../../components/Modal';
+import { Loader } from '../../components/Loader';
 
 export function Home() {
   return (
     <Container>
-      <Modal danger />
+      <Loader />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome" />

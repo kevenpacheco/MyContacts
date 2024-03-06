1ff78405efaf46a0ec2614b91cb342c118938272
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-03-06 15:30:47 -0300

    create Input and Select components

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,9 +1,20 @@
+import { Input } from '../../components/Input';
+import { Select } from '../../components/Select';
 import { PageHeader } from '../../components/PageHeader';
 
 export function NewContact() {
   return (
     <div>
       <PageHeader title="Novo contato" />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="1">Instagram</option>
+        <option value="1">Instagram</option>
+        <option value="1">Instagram</option>
+        <option value="1">Instagram</option>
+        <option value="1">Instagram</option>
+      </Select>
     </div>
   );
 }

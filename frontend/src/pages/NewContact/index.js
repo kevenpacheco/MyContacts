756bab404f395cdd90e0e540e78b05ce19b9c538
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-03-06 15:46:58 -0300

    create Button component

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import { Input } from '../../components/Input';
 import { Select } from '../../components/Select';
 import { PageHeader } from '../../components/PageHeader';
+import { Button } from '../../components/Button';
 
 export function NewContact() {
   return (
@@ -15,6 +16,12 @@ export function NewContact() {
         <option value="1">Instagram</option>
         <option value="1">Instagram</option>
       </Select>
+      <Button>
+        Salvar alterações
+      </Button>
+      <Button disabled>
+        Salvar alterações
+      </Button>
     </div>
   );
 }

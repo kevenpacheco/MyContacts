539a5b427c4b488f5e33651f908329dc39129e97
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-03-06 14:22:57 -0300

    create the home page and refactor the Header

--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  margin-top: 32px;
+export const Container = styled.div``;
+
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    background-color: "#fff";
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    outline: none;
+    padding: 0 16px;
+
+    &::placeholder {
+      color: #bcbcbc;
+    }
+  }
 `;
 
 export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 32px;
 
   strong {
     font-size: 24px;

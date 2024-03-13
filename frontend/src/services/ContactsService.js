84f1cf8cfc753c3f628d623179b11cd09c0ad25f
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-03-13 17:15:56 -0300

    create HttpClient

--- frontend/src/services/ContactsService.js
+++ frontend/src/services/ContactsService.js
@@ -1,12 +1,12 @@
-import { delay } from '../utils/delay';
+import { HttpClient } from './utils/HttpClient';
 
 class ContactsService {
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3333');
+  }
+
   async listContacts(orderBy = 'asc') {
-    const response = await fetch(`http://localhost:3333/contacts?orderBy=${orderBy}`);
-
-    await delay(2000);
-
-    return response.json();
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 }
 

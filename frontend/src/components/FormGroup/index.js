368f9ed58b930f5c40fbfd7ef5bb8d548154e15e
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-03-27 13:12:58 -0300

    componentizing the Spinner

--- frontend/src/components/FormGroup/index.js
+++ frontend/src/components/FormGroup/index.js
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types';
 
+import { Spinner } from '../Spinner';
+
 import { Container } from './styles';
 
 export function FormGroup({ children, error, isLoading }) {
-  console.log(isLoading);
   return (
     <Container>
       <div className="form-item">
         {children}
 
-        {isLoading && <div className="loader" />}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
 
       {error && <small>{error}</small>}

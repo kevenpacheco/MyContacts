db1facb7fae7808acef245eab8810657c0f48df0
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-05-08 17:13:41 -0300

    Replacing the CustomEvent with our EventManager

--- frontend/src/components/Toast/ToastContainer/index.js
+++ frontend/src/components/Toast/ToastContainer/index.js
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react';
 import { ToastMessage } from '../ToastMessage';
 import { Container } from './styles';
+import { toastEventManager } from '../../../utils/toast';
 
 export function ToastContainer() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    function handleAddToast({ type, text }) {
+      setMessages((prevState) => [
+        ...prevState,
+        { id: Math.random(), type, text },
+      ]);
+    }
+
+    toastEventManager.on('addtoast', handleAddToast);
+
+    return () => {
+      toastEventManager.removeListener('addtoast', handleAddToast);
+    };
+  }, []);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          text={message.text}
+          type={message.type}
+        />
+      ))}
     </Container>
   );
 }
--- frontend/src/lib/EventManager.js
+++ frontend/src/lib/EventManager.js
@@ -26,30 +26,10 @@ export default class EventManager {
       return;
     }
 
-    const filteredListeners = this.listeners.get(event).filter(
-      (listener) => listener !== listenerToRemove,
-    );
+    const filteredListeners = this.listeners
+      .get(event)
+      .filter((listener) => listener !== listenerToRemove);
 
     this.listeners.set(event, filteredListeners);
   }
 }
-
-const toastEventManager = new EventManager();
-
-function addtoast1(payload) {
-  console.log('addtoast listener1', payload);
-}
-
-function addtoast2(payload) {
-  console.log('addtoast listener2', payload);
-}
-
-toastEventManager.on('addtoast', addtoast1);
-toastEventManager.on('addtoast', addtoast2);
-toastEventManager.emit('addtoast', { type: 'danger', text: 'Opaa' });
-
-toastEventManager.removeListener('addtoast', addtoast1);
-
-toastEventManager.emit('addtoast', 'depois da remoção...');
-
-console.log({ toastEventManager });

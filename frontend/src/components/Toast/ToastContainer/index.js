b8e76296cd25dd0c25f744fc127ed59ec283fc0a
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-05-08 18:04:44 -0300

    Removendo os Toasts automaticamente após um período de tempo

--- frontend/src/components/Toast/ToastContainer/index.js
+++ frontend/src/components/Toast/ToastContainer/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { ToastMessage } from '../ToastMessage';
 import { Container } from './styles';
 import { toastEventManager } from '../../../utils/toast';
@@ -7,10 +7,12 @@ export function ToastContainer() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    function handleAddToast({ type, text }) {
+    function handleAddToast({ type, text, duration }) {
       setMessages((prevState) => [
         ...prevState,
-        { id: Math.random(), type, text },
+        {
+          id: Math.random(), type, text, duration,
+        },
       ]);
     }
 
@@ -21,9 +23,9 @@ export function ToastContainer() {
     };
   }, []);
 
-  function handleRemoveMessage(id) {
+  const handleRemoveMessage = useCallback((id) => {
     setMessages((prevState) => prevState.filter((message) => message.id !== id));
-  }
+  }, []);
 
   return (
     <Container>
--- frontend/src/components/Toast/ToastMessage/index.js
+++ frontend/src/components/Toast/ToastMessage/index.js
@@ -1,26 +1,35 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
 export function ToastMessage({ message, onRemoveMessage }) {
-  const { id, type, text } = message;
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 7000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [message, onRemoveMessage]);
 
   function handleRemove() {
-    onRemoveMessage(id);
+    onRemoveMessage(message.id);
   }
 
   return (
     <Container
-      type={type}
+      type={message.type}
       onClick={handleRemove}
       tabIndex={0}
       role="button"
     >
-      {type === 'danger' && <img src={xCircleIcon} alt="X" />}
-      {type === 'success' && <img src={checkCircleIcon} alt="Check" />}
-      <strong>{text}</strong>
+      {message.type === 'danger' && <img src={xCircleIcon} alt="X" />}
+      {message.type === 'success' && <img src={checkCircleIcon} alt="Check" />}
+      <strong>{message.text}</strong>
     </Container>
   );
 }
@@ -30,6 +39,7 @@ ToastMessage.propTypes = {
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     type: PropTypes.oneOf(['default', 'success', 'danger']),
+    duration: PropTypes.number,
   }).isRequired,
   onRemoveMessage: PropTypes.func.isRequired,
 };

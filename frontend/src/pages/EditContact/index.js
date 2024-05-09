50cdb0f0a5068f0c32f774be4ac39d805f132ebd
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-05-09 11:17:37 -0300

    Loading contact details on the edit page

--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,12 +1,51 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { ContactForm } from '../../components/ContactForm';
 import { PageHeader } from '../../components/PageHeader';
+import { Loader } from '../../components/Loader';
+import toast from '../../utils/toast';
+import ContactsService from '../../services/ContactsService';
 
 export function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        console.log(contactData);
+
+        setIsLoading(false);
+      } catch (error) {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader title="Editar Keven Pacheco" />
 
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }

538c0aec89b5c061fb066434a9778d44c342966d
Keven Pacheco <kevenpacheco2000@gmail.com>
2024-05-22 18:34:34 -0300

    releasing multiple origins in CORS

--- backend/src/app/middlewares/cors.js
+++ backend/src/app/middlewares/cors.js
@@ -1,7 +1,15 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+  const allowedCors = ["http://localhost:3000"];
+
+  const origin = request.headers("origin");
+  const isAllowed = allowedCors.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader("Access-Control-Allow-Origin", origin);
+    response.setHeader("Access-Control-Allow-Methods", "*");
+    response.setHeader("Access-Control-Allow-Headers", "*");
+    response.setHeader("Access-Control-Max-Age", "10");
+  }
+
   next();
 };
